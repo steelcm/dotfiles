f5cf49ff7beb274ab849b5a8312b54c5b05dcf7d
steelcm <csteel@gmail.com>
2014-05-23 09:16:33 +0100

    added more intelligent line numbering

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,7 @@
 (progn (menu-bar-mode 0))
 ;; general purpose defaults
 (line-number-mode t) ;; show line numbers in the status bar
+(load "~/.emacs.d/emacs_linum.el") ;; set's linum to not display on certain modes and *starred buffers
 (setq linum-format "%4d \u2502 ") ;; set space and bar seperator for margin line numbers
 (global-linum-mode t) ;; show line numbers
 
