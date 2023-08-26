3eae616803d651160f656ffa3b1444dd661d140e
Chris Steel <chris@Chriss-MacBook-Pro.local>
2023-08-26 12:28:17 +0100

    Playing around with transparent backgrounds.

--- nvim/.config/nvim/lua/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -4,5 +4,8 @@ return {
     priority = 1000,
     config = function()
         vim.cmd.colorscheme('tokyonight-night')
+        -- vim.api.nvim_set_hl(0, "Normal", { guibg = NONE, ctermbg = NONE })
+        -- vim.api.nvim_set_hl(0, "NeoTreeNormal", { guibg = NONE, ctermbg = NONE })
+        -- vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { guibg = NONE, ctermbg = NONE })
     end,
 }
