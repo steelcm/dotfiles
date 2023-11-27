7cba02bf7e74431487cc50be8f6e3b4f8a48338a
Chris Steel <info@steelcm.com>
2023-11-27 10:23:12 +0000

    Add gen plugin

---
+++ nvim/.config/nvim/lua/plugins/gen.lua
@@ -0,0 +1,9 @@
+return {
+    "David-Kunz/gen.nvim",
+    config = function()
+        vim.keymap.set('v', '<leader>gn', ':Gen<CR>')
+        vim.keymap.set('n', '<leader>gn', ':Gen<CR>')
+        require('gen').model = 'codellama'
+        -- require('gen').container = 'ollama'
+    end
+}
