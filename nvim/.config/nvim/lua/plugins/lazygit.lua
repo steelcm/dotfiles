6a6abae41d10d2c36ecab663d0b60a1d3f71a1b6
Chris Steel <info@steelcm.com>
2024-01-04 15:54:31 +0000

    Move lazygit config into keys

--- nvim/.config/nvim/lua/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -1,8 +1,10 @@
 -- https://github.com/kdheepak/lazygit.nvim
 return {
     'kdheepak/lazygit.nvim',
-    keys = { { '<leader>lg', desc = 'Git', mode = 'n' } },
-    config = function()
-        vim.keymap.set('n', '<leader>lg', ':LazyGit<CR>', { desc = '[S]ource [C]ontrol' })
-    end,
+    keys = { 
+        { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'Git', mode = 'n' } 
+    },
+    -- config = function()
+    --     vim.keymap.set('n', '<leader>lg', ':LazyGit<CR>', { desc = '[S]ource [C]ontrol' })
+    -- end,
 }
